690fe354494cc32ff637d659ee4ab8824ca17060
jotaerrec <javi.r49915@gmail.com>
2023-01-30 17:52:12 -0300

    Add projects

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import './App.css';
-import Home from './components/home/home.js'
-import About from './components/about/about.js'
+import React from "react";
+import "./App.css";
+import Home from "./components/home/home.js";
+import About from "./components/about/about.js";
+import Projects from "./components/projects";
 
 function App() {
   return (
-      <div>
-      <p className="copyright-text hidden md:flex text-gray-200 md:fixed -left-16 animate__animated animate__fadeIn">&copy; 2021 Javier Rodriguez</p>
-      <Home/> 
-      <About/> 
+    <div>
+      <p className="copyright-text hidden md:flex text-gray-200 md:fixed -left-16 animate__animated animate__fadeIn">
+        &copy; 2021 Javier Rodriguez
+      </p>
+      <Home />
+      <Projects />
+      <About />
     </div>
-    
   );
 }
 
---
+++ src/components/projects/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./project.css";
+
+const Projects = () => {
+  return (
+    <div className="block mt-10 md:mt-36">
+      <section className=" mx-auto max-w-7xl px-4">
+        <div className="text-center">
+          <h1 className="about stroke text-5xl mt-1.5 md:text-7xl uppercase">
+            Projects
+          </h1>
+        </div>
+
+        <div className="mx-auto w-6/12 bg-difuminado"></div>
+      </section>
+    </div>
+  );
+};
+
+export default Projects;
