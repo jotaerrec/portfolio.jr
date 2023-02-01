fc9e1a2ed5213dfef49e2f1b6969f0f358f142b3
jotaerrec <javi.r49915@gmail.com>
2023-02-01 18:38:41 -0300

    Updates

---
+++ precache-manifest.acb93dc908510ebc160ac325f889cc53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb24a304ab328b87498e",
+    "url": "/portfolio.jr/static/css/main.098bdd59.chunk.css"
+  },
+  {
+    "revision": "fb24a304ab328b87498e",
+    "url": "/portfolio.jr/static/js/main.fb24a304.chunk.js"
+  },
+  {
+    "revision": "061a6d486a72ec7002b1",
+    "url": "/portfolio.jr/static/js/1.061a6d48.chunk.js"
+  },
+  {
+    "revision": "12d37e54626e08bbc36d",
+    "url": "/portfolio.jr/static/js/2.12d37e54.chunk.js"
+  },
+  {
+    "revision": "b0e14675b30f2f8d6090",
+    "url": "/portfolio.jr/static/js/runtime~main.b0e14675.js"
+  },
+  {
+    "revision": "d3ee674f19b6f54a714735eeb26f8184",
+    "url": "/portfolio.jr/index.html"
+  }
+];
